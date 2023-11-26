0788297e9e9d324a894748889e1158d3033f43b5
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-26 18:23:23 +0900

    style: 냉장고 열기 버튼 css 변경

--- client/src/routes/refrigerator/OpenBtn.jsx
+++ client/src/routes/refrigerator/OpenBtn.jsx
@@ -1,9 +1,11 @@
+import { Button } from "@mui/material";
 import axios from "axios";
 
 const OpenBtn = ({ userId }) => {
   return (
     <>
       <button
+        className="absolute bottom-12 left-2/4 -translate-x-2/4 w-36 h-12 bg-primary/90 text-xl text-white rounded-xl"
         onClick={() => {
           let openFridgeUrl = `${process.env.REACT_APP_SERVER}/userid?${userId}`;
           axios?.get(openFridgeUrl)?.then((res) => {
