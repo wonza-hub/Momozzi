42c6a59fb3293990c2be5a139fdada85c992f788
Hong Beomsun <ghsqjatns@gmail.com>
2023-11-27 01:44:44 +0900

    model todo : id should not be created

--- server/api/models.py
+++ server/api/models.py
@@ -1,45 +1,58 @@
 from django.db import models
 
-# Create your models here.
 class User(models.Model):
     user_id = models.IntegerField(primary_key=True)
-    first_name = models.CharField(max_length=30, null=False)
-    last_name = models.CharField(max_length=30, null=False)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
     age = models.IntegerField()
 
 class Cuisine(models.Model):
-    cuisine_name = models.CharField(primary_key=True, max_length=30, null=False)
-    method = models.CharField(max_length=30, null=False)
-    category = models.CharField(max_length=30, null=False)
+    cuisine_name = models.CharField(primary_key=True, max_length=30)
+    method = models.CharField(max_length=30)
+    category = models.CharField(max_length=30)
 
 class Recipe(models.Model):
     recipe_id = models.IntegerField(primary_key=True, auto_created=True)
-    cook_time = models.IntegerField(null=False)
-    description = models.CharField(max_length=100, null=False)
-    process = models.TextField(null=False)
+    cook_time = models.IntegerField()
+    description = models.CharField(max_length=100)
+    process = models.TextField()
     cuisine_name = models.ForeignKey(Cuisine, on_delete=models.CASCADE)
 
 class Ingredient(models.Model):
-    ingredient_name = models.CharField(primary_key=True, max_length=30, null=False)
-    type = models.CharField(max_length=30, null=False)
+    ingredient_name = models.CharField(primary_key=True, max_length=30)
+    type = models.CharField(max_length=30)
     calories = models.IntegerField()
 
 class Review(models.Model):
     review_id = models.IntegerField(primary_key=True, auto_created=True)
-    content = models.TextField(null=False)
+    content = models.TextField()
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
 
-class Refrigerator(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, primary_key=True)
-    created_at = models.DateTimeField(auto_now_add=True, null=False,primary_key=True)
-    capacity = models.IntegerField(null=False)
-
 class Recipe_Needs_Ingredient(models.Model):
-    recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE, primary_key=True)
-    ingredient_name = models.ForeignKey(Ingredient, on_delete=models.CASCADE, primary_key=True)
+    recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient_name = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['recipe_id', 'ingredient_name'], name='unique_recipe_ingredient')
+        ]
+
+class Refrigerator(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    capacity = models.IntegerField()
     
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user_id', 'created_at'], name='unique_refrigerator')
+        ]
+
 class Refrigerator_Stores_Ingredient(models.Model):
-    user_id = models.ForeignKey(Refrigerator, on_delete=models.CASCADE, primary_key=True)
-    created_at = models.ForeignKey(Refrigerator, on_delete=models.CASCADE, primary_key=True)
-    ingredient_name = models.ForeignKey(Ingredient, on_delete=models.CASCADE, primary_key=True)
+    refrigerator = models.ForeignKey(Refrigerator, on_delete=models.CASCADE)
+    ingredient_name = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['refrigerator', 'ingredient_name'], name='unique_refrigerator_ingredient')
+        ]
