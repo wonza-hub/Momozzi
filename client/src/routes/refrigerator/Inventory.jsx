0ff7752af6123336980686095270a3f2eda7c14b
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-26 18:55:25 +0900

    feat: 냉장고 내 저장음식 목록 컴포넌트 추가

--- client/src/routes/refrigerator/Inventory.jsx
+++ client/src/routes/refrigerator/Inventory.jsx
@@ -1,5 +1,20 @@
-const Inventory = () => {
-  return <></>;
+import StoredIngredient from "./StoredIngredient";
+
+const Inventory = ({ storedIngredients }) => {
+  return (
+    <>
+      <header className="px-32 flex flex-row justify-between text-[18px] text-primary/80 font-bold mb-4">
+        <span>Type</span>
+        <span>Name</span>
+        <span>Calories</span>
+      </header>
+      <section className="w-[720px] max-h-[320px] px-32 py-2 border-[#E7EEF2] border-2 rounded-md overflow-y-auto">
+        {storedIngredients.map((storedIngredient) => (
+          <StoredIngredient ingredient={storedIngredient} />
+        ))}
+      </section>
+    </>
+  );
 };
 
 export default Inventory;
