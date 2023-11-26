94f1a00620e225ade900642cdd691dd37a63d6a9
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-26 18:22:01 +0900

    feat: 커스텀 색상 추가

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: "#435334",
+        secondary: "#9EB384",
+      },
+      textColor: {
+        primary: "#435334",
+        secondary: "#9EB384",
+      },
+    },
   },
   plugins: [],
 };
