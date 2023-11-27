8f30529ef1415d3330063b0d3dec5c700af83a0b
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-27 23:04:46 +0900

    feat: useLocation의 파라미터 여부에 따른 초기 api 분리

---
+++ client/src/routes/search/CuisineList.jsx
@@ -0,0 +1,10 @@
+import { Stack, Pagination } from "@mui/material";
+import { useState, useEffect } from "react";
+
+const CuisineList = ({ cuisineList }) => {
+  const [page, setPage] = useState(1);
+
+  return <></>;
+};
+
+export default CuisineList;
--- client/src/routes/search/CuisineSearcher.jsx
+++ client/src/routes/search/CuisineSearcher.jsx
@@ -1,13 +1,34 @@
 import SearchBar from "./SearchBar";
+import CuisineList from "./CuisineList";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 const CuisineSearcher = () => {
-  // const location = useLocation();
-  // const { method, category } = location.state.foodFilter;
+  const location = useLocation();
+  const foodFilter = location.state && location.state.foodFilter;
+  const { method, category } = foodFilter || {};
+
+  const [cuisines, setCuisines] = useState([]);
+
+  useEffect(() => {
+    if (method || category) {
+      const recommendUrl = `${process.env.REACT_APP_SERVER}/`;
+      axios?.get(recommendUrl)?.then((res) => {
+        setCuisines(res.data);
+      });
+    } else {
+      const dataUrl = `${process.env.REACT_APP_SERVER}/`;
+      axios?.get(dataUrl)?.then((res) => {
+        setCuisines(res.data);
+      });
+    }
+  }, [method, category]);
 
   return (
     <>
-      <SearchBar></SearchBar>
+      <SearchBar setCuisines={setCuisines}></SearchBar>
+      <CuisineList cuisines={cuisines}></CuisineList>
     </>
   );
 };
