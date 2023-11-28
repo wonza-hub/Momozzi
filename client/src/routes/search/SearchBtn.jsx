e3d54a14b10dacd9addcaafcdeea5606ca89c070
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-28 19:30:39 +0900

    fix: 검색버튼 클릭시 제출 이벤트 변경
    onSubmit->onClick

--- client/src/routes/search/SearchBtn.jsx
+++ client/src/routes/search/SearchBtn.jsx
@@ -1,9 +1,9 @@
-const SearchBtn = () => {
+const SearchBtn = ({ handleSearchClick }) => {
   return (
     <>
       <button
         className="bg-primary/80 text-white text-2xl font-bold w-60 h-12 mx-5 my-2 rounded-2xl"
-        type="submit"
+        onClick={handleSearchClick}
       >
         Search
       </button>
