fd3345b03c9f0fc1417bfc03f71e6b69efd9a82f
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-29 00:44:23 +0900

    style: 레시피 검색버튼 투명도 변경

--- client/src/routes/search/SearchBtn.jsx
+++ client/src/routes/search/SearchBtn.jsx
@@ -2,7 +2,7 @@ const SearchBtn = ({ handleSearchClick }) => {
   return (
     <>
       <button
-        className="bg-primary/80 text-white text-2xl font-bold w-60 h-12 mx-5 my-2 rounded-2xl"
+        className="bg-primary/60 text-white text-2xl font-bold w-60 h-12 mx-5 my-2 rounded-2xl"
         onClick={handleSearchClick}
       >
         Search
