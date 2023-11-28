56cbf4276f27cb725022f92dfc62df9b2c928d33
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-28 22:41:01 +0900

    feat: 레시피 썸네일 상세페이지 이동 링크 추가

--- client/src/routes/search/RecipePreview.jsx
+++ client/src/routes/search/RecipePreview.jsx
@@ -1,5 +1,6 @@
 // 테스트용 이미지
 import bg from "../../img/fridge_bg.png";
+import { Link } from "react-router-dom";
 
 const RecipePreview = ({ recipe }) => {
   const recipeThumbnail = recipe.thumbnail_url;
@@ -8,14 +9,21 @@ const RecipePreview = ({ recipe }) => {
 
   return (
     <>
-      <div className="w-[372px] h-[412px] pt-2 px-8 bg-white">
-        <div className="thumbnail h-[328px] rounded-2xl overflow-hidden shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
-          <img
-            className="w-full max-w-full h-full object-cover brightness-95"
-            src={bg}
-            alt={"recipeThumbnail"}
-          />
-        </div>
+      <div className="w-[296px] h-[416px] px-4 pt-4">
+        <Link
+          to={`../recipe/${recipe.recipe_id}`}
+          state={{
+            recipe,
+          }}
+        >
+          <div className="thumbnail h-[328px] rounded-2xl overflow-hidden shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+            <img
+              className="w-full max-w-full h-full object-cover brightness-95"
+              src={bg}
+              alt={"recipeThumbnail"}
+            />
+          </div>
+        </Link>
         <div className="pt-4 pl-2">
           <div className="description text-xl text-black">
             {recipeDescription}
