b1e5f95dd4026f27c020dc0971eb56b8fee7c15c
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-28 22:54:46 +0900

    feat: 레시피 상세 페이지 내 재료 컴포넌트 추가

--- client/src/routes/recipe/Ingredients.jsx
+++ client/src/routes/recipe/Ingredients.jsx
@@ -4,6 +4,13 @@ const Ingredients = ({ ingredients }) => {
       <header className="mb-5 text-3xl font-bold text-primary drop-shadow-md">
         Ingredients
       </header>
+      <div className="ingredients pb-2 mb-8 font-semibold text-xl text-white flex flex-row overflow-x-auto">
+        {ingredients.map((ingredient) => (
+          <div className="w-28 h-10 mr-6 bg-secondary text-center leading-10 rounded-2xl">
+            {ingredient}
+          </div>
+        ))}
+      </div>
     </>
   );
 };
