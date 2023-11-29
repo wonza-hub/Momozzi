7f43a60af3218f386afc38a40064699626899d6f
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-29 12:10:40 +0900

    style: 아웃라인 테마색상 적용

--- client/src/routes/search/SearchBox.jsx
+++ client/src/routes/search/SearchBox.jsx
@@ -7,7 +7,7 @@ const SearchBox = (
   return (
     <>
       <input
-        className="w-60 h-12 mx-5 my-2 px-6 text-xl font-bold rounded-2xl"
+        className="w-60 h-12 mx-5 my-2 px-6 text-xl font-bold rounded-2xl border-none outline-3 outline-primary"
         type={"text"}
         placeholder={"Cuisine name"}
         value={searchContent}
