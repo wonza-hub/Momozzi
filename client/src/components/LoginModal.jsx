758facbb37745e1e8d05518fa6f62df32382391c
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-12-02 18:35:11 +0900

    feat: 로그인 모달창 추가

--- client/src/components/LoginModal.jsx
+++ client/src/components/LoginModal.jsx
@@ -1,27 +1,95 @@
-const LoginModal = ({ isLoginOpen, handleLoginOpen }) => {
+import { useState, useRef } from "react";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import { loginUser } from "../reducer/userReducer";
+
+const LoginModal = ({ setIsLoginOpen }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const userDispatch = useDispatch();
+
+  const handleEmailInputChange = () => {
+    setEmail(emailInputRef.current.value);
+  };
+  const handlePasswordInputChange = () => {
+    setPassword(passwordInputRef.current.value);
+  };
+
+  const handleLoginSubmit = (event) => {
+    event.preventDefault();
+    if (!email) {
+      return alert("이메일을 입력하세요");
+    }
+    if (!password) {
+      return alert("비밀번호를 입력하세요");
+    }
+
+    let body = {
+      email,
+      password,
+    };
+
+    const loginPostURL = `${process.env.REACT_APP_SERVER}/api/user/login`;
+    axios
+      ?.post(loginPostURL, body, {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "http://localhost:3000",
+        },
+      })
+      ?.then((res) => {
+        console.log(body);
+        if (res.status === 200) {
+          userDispatch(loginUser(res.data));
+          setIsLoginOpen(false);
+        }
+      })
+      ?.catch((e) => {
+        console.log(e);
+        alert("로그인 실패");
+        setIsLoading(false);
+      });
+    setIsLoading(true);
+  };
+
   return (
     <>
       <div className="absolute w-screen h-screen left-0 top-0 bg-black/80 z-20">
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[485px] h-[700px] px-4 py-8 bg-white rounded-3xl brightness-95 shadow-[0_8px_30px_rgb(0,0,0,0.12)]">
           <div className="w-full h-full flex flex-col items-center">
             <header>
-              <h3 className="my-10 text-3xl text-primary tracking-wider">
-                LOGIN
+              <h3 className="my-10 text-2xl text-primary tracking-wider">
+                MOMOZZI
               </h3>
             </header>
             <div className="w-3/4">
-              <form className="w-full">
+              <form className="w-full" onSubmit={handleLoginSubmit}>
                 <input
+                  onChange={handleEmailInputChange}
                   className="w-full h-12 mb-5 px-8  bg-backgroundGray/50 rounded-lg focus:outline-primary"
                   type="text"
-                  placeholder={"ID"}
+                  value={email}
+                  placeholder={"Email"}
+                  ref={emailInputRef}
                 />
                 <input
+                  onChange={handlePasswordInputChange}
                   className="w-full h-12 mb-5 px-8  bg-backgroundGray/50 rounded-lg focus:outline-primary"
-                  type="text"
+                  type="password"
+                  value={password}
                   placeholder={"Password"}
+                  ref={passwordInputRef}
                 />
-                <button className="w-full h-12 my-5  bg-primary/90 rounded-md text-white text-lg focus:outline-primary">
+                <button
+                  className="w-full h-12 my-5 bg-primary/90 rounded-md text-white text-lg tracking-wider focus:outline-primary"
+                  type="submit"
+                  disabled={isLoading}
+                >
                   LOGIN
                 </button>
               </form>
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,11 +2,20 @@ import { Outlet, Link } from "react-router-dom";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { useState } from "react";
 import LoginModal from "./LoginModal";
+import { clearUser } from "../reducer/userReducer";
+import { useSelector, useDispatch } from "react-redux";
 
 const NavBar = () => {
   const [isLoginOpen, setIsLoginOpen] = useState(false);
   const handleLoginOpen = () => {
     setIsLoginOpen(!isLoginOpen);
+  };
+
+  const user = useSelector((state) => state.user);
+  const userDispatch = useDispatch();
+
+  const handleLogoutClick = () => {
+    userDispatch(clearUser());
   };
 
   return (
@@ -15,24 +24,33 @@ const NavBar = () => {
         <h1 className=" my-auto text-4xl font-semibold text-primary">
           <Link to={"/page"}>Momozzi</Link>
         </h1>
-        <div className="flex flex-end">
-          <button
-            className="text-xl mr-6 px-2 text-black/90"
-            onClick={handleLoginOpen}
-          >
-            Login
-          </button>
+        <div className="flex items-center flex-end">
+          {user.isLogin ? (
+            <>
+              <div className="w-12 h-12 mr-6 text-lg text-white text-center leading-[44px] rounded-full bg-secondary">
+                {user.name}
+              </div>
+              <button
+                className="text-xl mr-6 px-2 text-black/90"
+                onClick={handleLogoutClick}
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <button
+              className="text-xl mr-6 px-2 text-black/90"
+              onClick={handleLoginOpen}
+            >
+              Login
+            </button>
+          )}
           <button className="px-2">
             <RxHamburgerMenu className="text-3xl " />
           </button>
         </div>
       </div>
-      {isLoginOpen ? (
-        <LoginModal
-          isLoginOpen={isLoginOpen}
-          handleLoginOpen={handleLoginOpen}
-        />
-      ) : null}
+      {isLoginOpen ? <LoginModal setIsLoginOpen={setIsLoginOpen} /> : null}
       <Outlet />
     </>
   );
