a9f90d3708eab9814d4239552f598fce7ba1e1d6
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-12-03 20:21:46 +0900

    style: 주석 제거

--- client/src/routes/recipe/ReviewList.jsx
+++ client/src/routes/recipe/ReviewList.jsx
@@ -1,9 +1,6 @@
 import { memo } from "react";
 import Review from "./Review";
 
-// dummy
-// import { dummyReviews } from "../../constants/Constant";
-
 const ReviewList = ({ reviews }) => {
   return (
     <div className="h-[600px] pt-12 p-20 overflow-y-auto">
