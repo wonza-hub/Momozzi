ebfc6bbb0f45651c3518f3bf3f409c9d10c187de
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-12-03 20:19:12 +0900

    fix: 재료 컴포넌트 키 값 추가

--- client/src/routes/recipe/Ingredients.jsx
+++ client/src/routes/recipe/Ingredients.jsx
@@ -4,10 +4,13 @@ const Ingredients = ({ ingredients }) => {
       <header className="mb-5 text-3xl font-bold text-primary drop-shadow-sm">
         Ingredients
       </header>
-      <div className="ingredients pb-2 mb-8 font-semibold text-xl text-white flex flex-row overflow-x-auto">
+      <div className="ingredients mb-8 font-semibold text-xl text-white flex flex-row overflow-x-scroll">
         {ingredients.map((ingredient) => (
-          <div className="w-28 h-10 mr-6 bg-secondary text-center leading-10 rounded-2xl">
-            {ingredient}
+          <div
+            className="w-fit h-11 mr-6 px-2 bg-secondary text-center leading-10 rounded-2xl"
+            key={ingredient.id}
+          >
+            {ingredient.ingredient_name}
           </div>
         ))}
       </div>
