c283a06ff1ba2535a72b269f42ecb2d5e91659a6
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-12-04 22:29:44 +0900

    fix: 리뷰 키 추가

--- client/src/routes/recipe/Review.jsx
+++ client/src/routes/recipe/Review.jsx
@@ -1,7 +1,10 @@
 const Review = ({ review }) => {
   return (
     <>
-      <div className="w-full mb-4 py-3 px-5 bg-white rounded-3xl shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)]">
+      <div
+        className="w-full mb-4 py-3 px-5 bg-white rounded-3xl shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)]"
+        key={review.review_id}
+      >
         <div className="text-xl text-black/90">{review.content}</div>
         <div className="text-md text-black/90 text-right pr-4">
           {review.username}
