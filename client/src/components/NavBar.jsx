a1b978c759bbcbbc0622b8e3c6c6561266871b9c
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-12-05 16:58:01 +0900

    feat: 메뉴 드로어 구현

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,9 +1,20 @@
 import { Outlet, Link } from "react-router-dom";
 import { RxHamburgerMenu } from "react-icons/rx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import LoginModal from "./LoginModal";
 import { clearUser } from "../reducer/userReducer";
 import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
+
+// 메뉴 드로어 mui
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
 
 const NavBar = () => {
   const [isLoginOpen, setIsLoginOpen] = useState(false);
@@ -18,10 +29,71 @@ const NavBar = () => {
     userDispatch(clearUser());
   };
 
+  const [state, setState] = useState({
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  // 메뉴 드로어 목록
+  const list = (anchor) => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      {/* 로그인 불필요 */}
+      <List>
+        {[{ search: "레시피 검색" }].map((item, index) => {
+          const [key, value] = Object.entries(item)[0];
+
+          return (
+            <ListItem key={index} disablePadding>
+              <ListItemButton>
+                <Link to={key}>
+                  <ListItemText primary={value} />
+                </Link>
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
+      </List>
+      <Divider />
+      {/* 로그인 필요 */}
+      {user.isLogin ? (
+        <List>
+          {[{ mypage: "내 프로필" }].map((item, index) => {
+            const [key, value] = Object.entries(item)[0];
+
+            return (
+              <ListItem key={index} disablePadding>
+                <ListItemButton>
+                  <Link to={key}>
+                    <ListItemText primary={value} />
+                  </Link>
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        </List>
+      ) : null}
+    </Box>
+  );
+
   return (
     <>
       <div className="fixed w-full h-[100px] px-8 bg-white/95 top-0 z-10 flex flex-row justify-between">
-        <h1 className=" my-auto text-4xl font-semibold text-primary">
+        <h1 className="my-auto text-4xl font-semibold text-primary">
           <Link to={"/page"}>Momozzi</Link>
         </h1>
         <div className="flex items-center flex-end">
@@ -46,7 +118,16 @@ const NavBar = () => {
             </button>
           )}
           <button className="px-2">
-            <RxHamburgerMenu className="text-3xl " />
+            <Button onClick={toggleDrawer("right", true)}>
+              <RxHamburgerMenu className="text-3xl" />
+            </Button>
+            <Drawer
+              anchor="right"
+              open={state["right"]}
+              onClose={toggleDrawer("right", false)}
+            >
+              {list("right")}
+            </Drawer>
           </button>
         </div>
       </div>
