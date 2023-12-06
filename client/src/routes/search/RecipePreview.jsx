a1e6127f9a0bb68e28e394e1b8fc23c4c1b46dd0
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-12-06 21:41:14 +0900

    style: 레시피 썸네일 레이아웃 변경

--- client/src/routes/search/RecipePreview.jsx
+++ client/src/routes/search/RecipePreview.jsx
@@ -12,22 +12,24 @@ const RecipePreview = ({ recipe }) => {
 
   return (
     <>
-      <div className="w-[296px] h-[416px] px-4 pt-4">
+      <div className="w-[296px] h-[400px] px-4 pt-4">
         <Link to={`../recipe/${recipeId}`}>
-          <div className="thumbnail h-[328px] rounded-2xl overflow-hidden shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] hover:-translate-y-1 duration-200 ease-in-out">
+          <div className="thumbnail h-[360px] rounded-2xl overflow-hidden shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] hover:-translate-y-1 duration-200 ease-in-out">
             <img
-              className="w-full max-w-full h-full object-cover contrast-120 brightness-95"
+              className="w-full max-w-full h-3/4 object-cover contrast-120 brightness-95"
               src={`/img/${recipeId}.png`}
               alt={"recipeThumbnail"}
             />
+            <div className="pt-4 pl-2">
+              <div className="description p-1 text-xl text-black whitespace-nowrap overflow-hidden text-ellipsis">
+                {recipeDescription}
+              </div>
+              <div className="cuisineName pr-3 text-right text-sm text-black">
+                {cuisineName}
+              </div>
+            </div>
           </div>
         </Link>
-        <div className="pt-4 pl-2">
-          <div className="description text-xl text-black whitespace-nowrap overflow-hidden text-ellipsis">
-            {recipeDescription}
-          </div>
-          <div className="cuisineName text-l text-black">{cuisineName}</div>
-        </div>
       </div>
     </>
   );
