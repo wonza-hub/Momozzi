e6787b607f8dafc8d234a021d2571cbff8d3b834
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-12-08 12:49:59 +0900

    feat: 로그인 정보 로컬스토리지 생성 추가

--- client/src/reducer/userReducer.js
+++ client/src/reducer/userReducer.js
@@ -1,5 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const loadUserFromLocalStorage = () => {
+  try {
+    const user = localStorage.getItem("user");
+    return user ? JSON.parse(user) : null;
+  } catch (error) {
+    console.error("Error loading user from localStorage:", error);
+    return null;
+  }
+};
+
 export const userReducer = createSlice({
   name: "user",
   initialState: {
@@ -15,15 +25,32 @@ export const userReducer = createSlice({
       state.id = action.payload[0].user_id;
       state.name = state.first_name + state.last_name;
       state.isLogin = true;
+
+      // Local Storage에 로그인정보 저장
+      localStorage.setItem("user", JSON.stringify(action.payload[0]));
     },
 
     clearUser: (state) => {
       state.name = "";
       state.id = "";
       state.isLogin = false;
+
+      // Local Storage의 로그인정보 삭제
+      localStorage.removeItem("user");
+    },
+
+    loadUser: (state) => {
+      const user = loadUserFromLocalStorage();
+      if (user) {
+        state.first_name = user.first_name;
+        state.last_name = user.last_name;
+        state.id = user.user_id;
+        state.name = user.first_name + user.last_name;
+        state.isLogin = true;
+      }
     },
   },
 });
 
-export const { loginUser, clearUser } = userReducer.actions;
+export const { loginUser, clearUser, loadUser } = userReducer.actions;
 export default userReducer;
