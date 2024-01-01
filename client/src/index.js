58dd2d57d4518cf92eb16f4933f05a02b8da2f2e
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-02 00:13:49 +0900

    feat: 리액트 쿼리 세팅 추가

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,17 @@ import App from "./App";
 import "./input.css";
 import store from "./store";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <App />
+      <ReactQueryDevtools />
+    </Provider>
+  </QueryClientProvider>
 );
